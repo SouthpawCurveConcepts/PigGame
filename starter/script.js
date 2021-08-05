ea056eb5968ffa4536aab4ca527a5b5fd0b23cbb
Jay For Short <jagoblacksky@gmail.com>
2021-08-05 14:03:55 -0500

    83. Roll dice functionality

--- starter/script.js
+++ starter/script.js
@@ -1,3 +1,26 @@
 'use strict';
 
 console.log('Hello pig game!');
+
+// Selecting elements
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn-roll');
+const btnHold = document.querySelector('.btn--hold');
+
+// Starting conditions
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
+
+// Roll dice functionality
+btnRoll.addEventListener('click', function () {
+  // 1.
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  // 2.
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
+  // 3.
+});
