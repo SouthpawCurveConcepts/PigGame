22511b1d4f9346d35625d469dab43dd749ff5ab0
Jay For Short <jagoblacksky@gmail.com>
2021-08-07 11:45:41 -0500

    84.  Switch active player done

--- starter/script.js
+++ starter/script.js
@@ -3,24 +3,47 @@
 console.log('Hello pig game!');
 
 // Selecting elements
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+
 const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
-const btnRoll = document.querySelector('.btn-roll');
+const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
 // Starting conditions
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
+let currentScore = 0;
+let activePlayer = 0;
 
 // Roll dice functionality
 btnRoll.addEventListener('click', function () {
   // 1.
   const dice = Math.trunc(Math.random() * 6) + 1;
+  console.log(dice);
   // 2.
   diceEl.classList.remove('hidden');
   diceEl.src = `dice-${dice}.png`;
   // 3.
+  if (dice !== 1) {
+    currentScore += dice;
+    // Change based on which player is active right now
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+    // current0El.textContent = currentScore;
+  } else {
+    // Switch
+    currentScore = 0;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
+  }
 });
